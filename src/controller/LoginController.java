90a1a8e426d64615c74a5286593704de96a567b8
adriDiazz <adriandiazmanzanares9@gmail.com>
2021-10-17 16:20:40 +0200

    Cambios a la GUI

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -48,7 +48,7 @@ public class LoginController {
     private Hyperlink forgotTF;
 
     @FXML
-    private JFXButton newBtn;
+    private Hyperlink newBtn;
     
     
     private Stage mainWindow;
